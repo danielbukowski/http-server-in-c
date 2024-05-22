fd8833ac093d184f5b00b680ae8f84e740513fd4
danielbukowski <daniel.bukowski01@gmail.com>
2024-05-22 18:58:34 +0200

    Update the queue header file

--- circular_queue.h
+++ circular_queue.h
@@ -1,16 +1,12 @@
-#include <stdio.h>
-#include <stdbool.h>
-
-#define QUEUE_SIZE 10
 
 typedef struct circular_queue circular_queue;
 
-bool isFull(circular_queue* queue);
+static bool isFull(circular_queue* queue);
 
-bool isEmpty(circular_queue* queue);
+static bool isEmpty(circular_queue* queue);
 
 circular_queue* init_queue();
 
 bool enqueue(circular_queue* queue, int element);
 
-bool dequeue(circular_queue* queue);
+int dequeue(circular_queue* queue);
