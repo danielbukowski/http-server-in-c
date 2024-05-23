da2f79aa10958584092043f56074780b5f21bfe7
danielbukowski <daniel.bukowski01@gmail.com>
2024-05-23 22:47:05 +0200

    Add implementation of a circular queue

---
+++ circular_queue.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "circular_queue.h"
+
+#define QUEUE_SIZE 175
+
+typedef struct circular_queue {
+	int elements[QUEUE_SIZE];
+	int front;
+	int rear;
+} circular_queue;
+
+static bool isFull(circular_queue* queue) 
+{
+	return ((queue->front == queue->rear + 1) || (queue->front == 0 && queue->rear == QUEUE_SIZE - 1));
+}
+
+static bool isEmpty(circular_queue* queue) 
+{
+	return (queue->front == -1);
+}
+
+circular_queue* init_queue() 
+{
+	circular_queue* queue = malloc(sizeof(circular_queue));
+
+	if (queue == NULL) return NULL;
+
+	queue->front = -1;
+	queue->rear = -1;
+
+	return queue;
+}
+
+bool enqueue(circular_queue* queue, int element) 
+{
+	if (isFull(queue))
+		return false;
+
+	if (queue->front == -1) queue->front = 0;
+
+	queue->rear = (queue->rear + 1) % QUEUE_SIZE;
+	queue->elements[queue->rear] = element;
+
+	return true;
+}
+
+int dequeue(circular_queue* queue) 
+{
+	if (isEmpty(queue))
+	{
+		return -1;
+	}
+
+	int element = queue->elements[queue->front];
+
+	if (queue->front == queue->rear) 
+	{
+		queue->front = -1;
+		queue->rear = -1;
+		return element;
+	}
+
+	queue->front = (queue->front + 1) % QUEUE_SIZE;
+
+	return element;
+}
